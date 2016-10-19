998234b96bb82308f07b4b3cd77a853b567cf8cc
yuhi <atsui05@yahoo.co.jp>
2016-10-18 21:00:53 -0700

    user

---
+++ fake/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  fake
+//
+//  Created by 大谷勇陽 on 2016/10/18.
+//  Copyright © 2016年 yu_hi. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    
+    var name : String = ""
+    var color = UIColor()
+    var id : Int = 0
+    var fakeartist : Bool = false
+    
+}
